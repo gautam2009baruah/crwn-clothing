f6f4c33f75daece19e56d03b078b874558aefd9e
Baruah, Gautam <Gautam.Baruah@sabre.com>
2021-07-19 12:39:19 +0530

    2nd day

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './homepage.component'
+import HomePage from './Pages/HomePage/homepage.component'
 
 function App() {
   return (
